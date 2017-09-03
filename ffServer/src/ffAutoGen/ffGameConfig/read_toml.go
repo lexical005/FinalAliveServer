de9f3e839f2078b48afbda185fb1874a160453d7
yyk <yyk@qq.com>
2017-09-03 18:07:46 +0800

    ffGameConfig 优化

---
+++ ffServer/src/ffAutoGen/ffGameConfig/read_toml.go
@@ -0,0 +1,41 @@
+package ffGameConfig
+
+import (
+	"ffCommon/log/log"
+)
+
+// ExcelExportTestData 配置表ExcelExportTest的数据
+var ExcelExportTestData *ExcelExportTest
+
+// ItemData 配置表Item的数据
+var ItemData *Item
+
+// LanguageData 配置表Language的数据
+var LanguageData *Language
+
+// ReadAllToml 读取所有toml配置
+func ReadAllToml() (result bool) {
+	result = true
+
+	var err error
+
+	ExcelExportTestData, err = ReadExcelExportTest()
+	if err != nil {
+		result = false
+		log.FatalLogger.Printf("ReadExcelExportTest get error[%v]", err)
+	}
+
+	ItemData, err = ReadItem()
+	if err != nil {
+		result = false
+		log.FatalLogger.Printf("ReadItem get error[%v]", err)
+	}
+
+	LanguageData, err = ReadLanguage()
+	if err != nil {
+		result = false
+		log.FatalLogger.Printf("ReadLanguage get error[%v]", err)
+	}
+
+	return result
+}
